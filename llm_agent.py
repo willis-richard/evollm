1d048a8e9d43c4711731a6a43d927bb51597130f
Muff2n <richard.willis5@gmail.com>
2024-07-08 15:53:09 +0100

    Think I was trying to write my own agents rather than use the concordia library

--- llm_agent.py
+++ llm_agent.py
@@ -3,6 +3,9 @@ import logging
 import axelrod as axl
 from concordia.language_model.language_model import (InvalidResponseError,
                                                      LanguageModel)
+from concordia.language_model.gpt_model import GptLanguageModel
+
+logger = logging.getLogger(__name__)
 
 
 class Agent(axl.player.Player):
@@ -24,45 +27,61 @@ class Agent(axl.player.Player):
   #   "cooperative": "Your objective is to maximise two thirds times your total payoffs plus one third of the payoff of your opponent received in the iterated game. For example, if you score 12, and your opponent scores 24, your overall score is 16. Expressed as a function, your reward function is 2/3 * R_you + 1/3 * R_opponent, where R is the sum of the rewards in the iterated game.",
   # }
   reward_prompts = {
-    "selfish": "You are a selfish, rational agent. Your objective is to maximise your total payoffs received in the iterated game. The payoffs to your opponent do not matter.",
-    "adversarial": "You are a competitive, rational agent. Your objective is to maximise your total payoffs received in the iterated game. You should also aim to outperform your opponent, by achieving a higher payoff than them.",
-    "cooperative": "You are a cooperative, rational agent. Your objective is to maximise your total payoffs received in the iterated game. You should also help your opponent achieve a good total payoff.",
+      "selfish":
+          "You are a selfish, rational agent. Your objective is to maximise your total payoffs received in the iterated game. The payoffs to your opponent do not matter.",
+      "adversarial":
+          "You are a competitive, rational agent. Your objective is to maximise your total payoffs received in the iterated game. You should also aim to outperform your opponent, by achieving a higher payoff than them.",
+      "cooperative":
+          "You are a cooperative, rational agent. Your objective is to maximise your total payoffs received in the iterated game. You should also help your opponent achieve a good total payoff.",
   }
 
-  def __init__(self, model: LanguageModel, attitude: str):
+  def __init__(self, model: LanguageModel, attitude: str, seed: int):
     super().__init__()
     self._model = model
     self._attitude = attitude
     self._reward_prompt = self.reward_prompts[attitude]
-    self._n_errors = 0
-
+    self._max_tries = 3
+    self.seed = seed
 
   def strategy(self, opponent: axl.player.Player) -> axl.Action:
-    self._game_prompt = self.match_attributes["game"].game_prompt_fn(self.match_attributes["length"])
+    self._game_prompt = self.match_attributes["game"].game_prompt_fn(
+        self.match_attributes["length"])
 
     # Not first move
     if not self.history:
       state_prompt = "This is the first round."
     else:
-      state_prompt = self.match_attributes["game"].state_prompt_fn(self, opponent)
+      state_prompt = self.match_attributes["game"].state_prompt_fn(
+          self, opponent)
 
-    prompt = "\n".join([self._game_prompt, self._reward_prompt, state_prompt,
-        "What action do you want to take?"
+    prompt = "\n".join([
+        self._game_prompt, self._reward_prompt, state_prompt,
+        "What action do you want to take? Respond with EXACTLY C or D."
     ])
 
-    try:
-      logging.info("Sending API call with prompt:\n%s", prompt)
-      output = self._model.sample_choice(prompt, ["C", "D"])[1]
-      logging.info("Received response: %s", output)
-    except InvalidResponseError:
-      self._n_errors += 1
-      logging.error(
-          "Language model did not respond with 'C' or 'D' after retries. Total errors so far %s out of %s rounds. Choosing 'C'.",
-          self._n_errors, len(self.history))
+    success = False
+    logger.info("Sending API call to model %s with prompt:\n%s", self.__repr__(),
+                prompt)
+    for temp in [0, 0.3, 0.6]:
+      output = self._model.sample_text(prompt, max_tokens=1, temperature=temp, seed=self.seed)
+      if output in ["C", "D"]:
+        logger.info("Received response: %s", output)
+        success = True
+        break
+      else:
+        logger.warn("Received response: %s", output)
+
+    if not success:
+      logger.error(
+          "Language model did not respond with 'C' or 'D' after retries. Choosing 'C'."
+      )
       output = "C"
 
     return axl.Action.from_char(output)
 
   def __repr__(self):
-    # return f"{self.name}({self._model._model_name})({self._attitude})"
-    return f"{self.name}({self._attitude})"
+    if type(self._model) == GptLanguageModel:
+      model_name = self._model._model_name.split("_")[0]
+    else:
+      assert False, "Implement model name for this model type"
+    return f"{self.name}({model_name})({self._attitude})"
--- main.py
+++ main.py
@@ -12,31 +12,38 @@ from fixed_tournament import _play_matches_mixed, _build_tasks_including_mirror
 from games import Chicken
 from llm_agent import Agent
 
+# Configure logging
+logging.basicConfig(filename="llm.log", filemode="w", level=logging.INFO)
+
+logging.getLogger("openai._base_client").setLevel(logging.WARN)
+logging.getLogger("httpx").setLevel(logging.WARN)
+
 # monkey patch methods
 axl.Tournament._play_matches = _play_matches_mixed
 axl.ResultSet._build_tasks = _build_tasks_including_mirror
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-# logging.getLogger("openai._base_client").setLevel(logging.DEBUG)
-
 CHAT_KEY = os.environ["OPENAI_API_KEY"]
 
-model = GptLanguageModel(
-    CHAT_KEY,
-    "gpt-3.5-turbo",
-)
+if __name__ == "__main__":
+  model = GptLanguageModel(
+      CHAT_KEY,
+      # "gpt-4o",
+      "gpt-3.5-turbo",
+  )
 
-reward_prompts = ["selfish", "adversarial", "cooperative"]
+  reward_prompts = ["selfish", "adversarial", "cooperative"]
 
-players = [Agent(model, rp) for rp in reward_prompts]
-chicken = Chicken()
-tournament = axl.Tournament(
-    players, game=chicken, turns=20, noise=0.1, repetitions=1, seed=1)
-# prob_end=0.05,
+  seed = 1
 
-results = tournament.play(processes=None, filename="results_full.txt")
+  players = [Agent(model, rp, seed) for rp in reward_prompts]
+  chicken = Chicken()
+  tournament = axl.Tournament(
+      players, game=chicken, turns=20, repetitions=1, seed=seed)
+  # prob_end=0.05,
+  # noise=0.1,
 
-df = pd.DataFrame(results.summarise()).set_index("Rank", drop=True)
-print(df)
-results.write_summary("results_summary.txt")
+  results = tournament.play(processes=4, filename="results_full.txt")
+
+  df = pd.DataFrame(results.summarise()).set_index("Rank", drop=True)
+  print(df)
+  results.write_summary("results_summary.txt")
